a43dd0afe79033b111941b28fda17c6f5959894b
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-19 10:46:04 -0400

    Add files via upload

---
+++ Q1A3.java
@@ -0,0 +1,29 @@
+package A3;
+
+import java.util.Scanner;
+import java.lang.Math;
+public class Q1A3 {
+    public static void main (String [] args){
+        Scanner input = new Scanner (System.in);
+
+        //input
+        System.out.println("Enter the manufacturing cost: ");
+        double manuf_cost = input.nextInt();
+        System.out.println("Enter the selling cost: ");
+        double sell_cost = input.nextInt();
+        System.out.println("Enter the fixed cost: ");
+        double fix_cost = input.nextInt();
+        System.out.println("Enter the selling price: ");
+        double sell_price = input.nextInt();
+
+        //Base on the question, we have the below formula:
+        //total cost = (manufacturing cost + selling cost)* units + fixed cost;
+        //earn = selling price * units;
+
+        //break-even point --> total_cost == earn
+        double num_of_units = fix_cost/(sell_price - (manuf_cost + sell_cost));
+        System.out.println("The number of units it will take to reach the break-even point is " + (int)Math.ceil(num_of_units) + " units.");
+
+
+    }
+}
---
+++ Q2A3.java
@@ -0,0 +1,30 @@
+package A3;
+
+import java.util.Scanner;
+public class Q2A3 {
+    public static void main (String [] args){
+        Scanner input = new Scanner (System.in);
+
+        //Initialize stage.
+        int largest_num = 0;
+        int count = 1;          //count = 1 because each integer will appear at least 1 time.
+                                //If I set count = 0, assume a number appear 2 times will only be counted 1 time.
+
+        //Loop through user input
+        for (int i = 1; i <=5; i++){
+        System.out.println("Enter the " + i + " integer: ");
+        int num = input.nextInt();
+
+        //Find the largest number
+        if (num > largest_num)
+            largest_num = num;
+
+        //Count the repeated number
+        else if (num == largest_num)
+            count++;
+        }
+        System.out.println("The largest number is " + largest_num);
+        System.out.println("The number " + largest_num + " appeared " + count + " times.");
+
+    }
+}
